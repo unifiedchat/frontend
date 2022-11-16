c9fb129a4021eb6538936d4d044770087f5599e8
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-11-16 17:46:44 +0300

    feat: sidebar

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,11 +1,31 @@
-import YouTubeIcon from '@mui/icons-material/YouTube';
-
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 //https://mui.com/material-ui/material-icons
 
 export default function Sidebar() {
-    return (
-        <div className="flex justify-center p-4 w-20 h-full fixed left-0 bg-[#131c2e]">
-            <YouTubeIcon className="text-white" />
+  return (
+    <div className="flex justify-between py-4 w-20 hover:w-40 h-full duration-300	 fixed left-0 flex-col bg-[#131c2e]">
+      <div className="flex justify-center">
+        <div>
+          <GitHubIcon className="text-white" />
         </div>
-    )
+      </div>
+      <div className="flex justify-start flex-col">
+        <div className="px-4 flex justify-between items-center rounded-r-xl w-30 h-8 bg-[#080D15]">
+          <div>
+            <YouTubeIcon className="text-white" />
+          </div>
+          <div>
+            <h1 className="text-white hover:none">sa</h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div>
+          <PeopleAltIcon className="text-white " />
+        </div>
+      </div>
+    </div>
+  );
 }
