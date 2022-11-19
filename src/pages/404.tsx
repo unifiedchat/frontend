e5cb7ffdd074a990a52c2381a54d6dc750469750
Barış DEMİRCİ <demirci.baris@gmail.com>
2022-11-20 00:57:08 +0300

    🔒️ Add router as dependency

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,13 @@
-import {NextPage} from "next";
-import {useEffect} from "react";
-import {useRouter} from "next/router";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function NotFound() {
     const router = useRouter();
 
     useEffect(() => {
         router.push("/");
-    }, []);
+    }, [router]);
 
     return (
         <div className="flex justify-center items-center h-[100vh] text-3xl">
