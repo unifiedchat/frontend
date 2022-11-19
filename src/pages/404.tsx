2aa0fa3435fdcfac9ed322aec37cdbeb1179b0fa
risy <therisyy@gmail.com>
2022-11-19 14:01:55 +0300

    -

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,14 @@
 import {NextPage} from "next";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 export default function NotFound() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/");
+    }, []);
+
     return (
         <div className="flex justify-center items-center h-[100vh] text-3xl">
             <h1>Not found.</h1>
