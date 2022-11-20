ab2228ab657ce90fb6465ca0e297459c889eb467
risy <therisyy@gmail.com>
2022-11-20 17:20:36 +0300

    🎨

--- src/core/layout/SideBar.tsx
+++ src/core/layout/SideBar.tsx
@@ -1,107 +1,119 @@
-import { Twitter } from "@mui/icons-material";
+import {HomeMax, HomeMini, Settings, Twitter} from "@mui/icons-material";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { Typography } from "@mui/material";
+import {Typography} from "@mui/material";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { ReactNode, useEffect, useState } from "react";
+import HomeIcon from '@mui/icons-material/Home';
+import {useRouter} from "next/router";
+import {ReactNode, useEffect, useState} from "react";
 import TokenService from "../../services/TokenService";
+import Button from "@mui/material/Button";
 
 //https://mui.com/material-ui/material-icons
 
-export default function SideBar({ children }: { children: ReactNode }) {
-	const router = useRouter();
-	const [token, setToken] = useState<string>("");
-	const clsName =
-		"text-4xl text-white hover:rotate-[360deg] transition-all duration-300  ease-in-out";
-	const routers = [
-		{
-			name: "Youtube",
-			icon: <YouTubeIcon className={clsName} />,
-			href: "/youtube",
-		},
-		{
-			name: "Twitch",
-			icon: <Twitter className={clsName} />,
-			href: "/twitch",
-		},
-	];
+export default function SideBar({children}: { children: ReactNode }) {
+    const router = useRouter();
+    const [token, setToken] = useState<string>("");
+    const clsName =
+        "text-4xl text-white hover:rotate-[360deg] transition-all duration-300  ease-in-out";
+    const routers = [
+        {
+            name: "Home",
+            icon: <HomeIcon className={clsName}/>,
+            path: "/",
+        },
+        {
+            name: "Youtube",
+            icon: <YouTubeIcon className={clsName}/>,
+            href: "/youtube",
+        },
+        {
+            name: "Twitch",
+            icon: <Twitter className={clsName}/>,
+            href: "/twitch",
+        },
+    ];
 
-	function redirectToPage(page: string) {
-		router.push(page);
-	}
+    function redirectToPage(page: string) {
+        router.push(page);
+    }
 
-	useEffect(() => {
-		const accessToken = TokenService.getToken();
+    useEffect(() => {
+        const accessToken = TokenService.getToken();
 
-		if (accessToken) setToken(accessToken);
-	}, []);
+        if (accessToken) setToken(accessToken);
+    }, []);
 
-	return (
-		<div>
-			<div className="flex justify-between lg:w-60 w-20 h-full duration-300 fixed left-0 flex-col bg-[#131c2e]">
-				<div className="flex flex-col justify-between items-center">
-					<div className="flex justify-center items-center border-b border-gray-700 h-14 w-full">
-						<Image
-							src="https://avatars.githubusercontent.com/u/118222787?s=200&v=4"
-							alt="asd"
-							className="flex w-10 h-10 rounded-full"
-							width={200}
-							height={200}
-						/>
-						<Typography variant="h5" className="text-white ml-2">
-							UnifiedChat
-						</Typography>
-					</div>
+    return (
+        <div>
+            <div className="flex justify-between w-72 h-full duration-300 fixed left-0 flex-col bg-[#131c2e]">
+                <div className="flex flex-col justify-between items-center">
+                    <div className="flex justify-center items-center h-24 w-full cursor-pointer">
+                        <Image
+                            src="https://avatars.githubusercontent.com/u/118222787?s=200&v=4"
+                            alt="asd"
+                            className="flex w-12 h-12 rounded-full"
+                            width={200}
+                            height={200}
+                        />
+                        <Typography variant="h5" className="text-white ml-4">
+                            UnifiedChat
+                        </Typography>
+                    </div>
 
-					<div className="flex justify-center flex-col gap-4 mt-6 w-full">
-						{routers.map((router, index) => (
-							<div
-								className="p-6 flex items-center lg:rounded-r-xl w-30 h-8 hover:bg-[#20345a] cursor-pointer"
-								key={index}
-							>
-								<div className="text-center">{router.icon}</div>
-								<div>
-									<h1 className="text-white ml-2 hover:none lg:flex hidden">
-										{router.name}
-									</h1>
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-				{!token && (
-					<div
-						className="flex items-center justify-center border-t border-gray-700 h-14 cursor-pointer hover:bg-[#20345a]"
-						onClick={() => redirectToPage("login")}
-					>
-						<PeopleAltIcon className="text-white" />
-						<span className="text-white ml-2 lg:flex hidden">
-							Login
-						</span>
-					</div>
-				)}
+                    <div className="flex justify-center flex-col gap-4 mt-6 w-full">
+                        {routers.map((r, index) => (
+                            <div
+                                className={`p-6 flex items-center w-30 h-8 hover:bg-[#20345a] cursor-pointer ` + (r.path === router.pathname ? "bg-[#20345a]" : "")}
+                                key={index}
+                            >
+                                <div className="text-center">{r.icon}</div>
+                                <div>
+                                    <h1 className="text-white ml-2 hover:none lg:flex hidden">
+                                        {r.name}
+                                    </h1>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {token && (
+                    <div
+                        className="flex w-full items-center justify-center h-14 cursor-pointer"
+                        onClick={() => redirectToPage("login")}
+                    >
+                        <Button variant="contained" color="primary" fullWidth={true}>
+                            Login
+                        </Button>
+                    </div>
+                )}
 
-				{token && (
-					<div
-						className="flex items-center justify-center border-t border-gray-700 h-14 cursor-pointer hover:bg-[#20345a]"
-						onClick={() => redirectToPage("profile")}
-					>
-						<Image
-							src="https://avatars.githubusercontent.com/u/14241866?v=4"
-							alt="Avatar"
-							width={40}
-							height={40}
-							className="rounded-full"
-						/>
-						<span className="text-white ml-2 lg:flex hidden">
-							Risy
-						</span>
-					</div>
-				)}
-			</div>
+                {!token && (
+                    <div
+                        className="flex items-center cursor-pointer p-4"
+                        onClick={() => redirectToPage("profile")}
+                    >
+                        <div className="flex justify-between w-full items-center mx-2">
+                            <div className="flex items-center w-full">
+                                <Image
+                                    src="https://avatars.githubusercontent.com/u/14241866?v=4"
+                                    alt="Avatar"
+                                    width={40}
+                                    height={40}
+                                    className="rounded-full"
+                                />
+                                <Typography variant="h6" className="text-white !font-normal ml-2">
+                                    Risy
+                                </Typography>
+                            </div>
 
-			<div className="ml-20">{children}</div>
-		</div>
-	);
+                            <Settings className={clsName + " !text-3xl"}/>
+                        </div>
+                    </div>
+                )}
+            </div>
+
+            <div className="ml-20">{children}</div>
+        </div>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import { Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
+	function handleLogin() {
+		const URL = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=http://localhost:3001/auth/youtube/callback&scope=https://www.googleapis.com/auth/youtube.readonly&client_id=918708795391-kapslmfl70k5rgrsm30819sugd6g0vli.apps.googleusercontent.com&service=lso&o2v=2&flowName=GeneralOAuthFlow&access_type=offline&include_granted_scopes=true"
+		window.open(URL, "_blank")
+	}
+
 	return (
 		<div>
 			<Head>
@@ -14,8 +19,8 @@ export default function Home() {
 			</Head>
 
 			<main className="flex flex-col justify-center items-center h-[100vh] w-full text-black">
-				<Typography variant="h3">
-					Create Connection for live chat of youtube
+				<Typography variant="h3"  onClick={handleLogin} >
+					Hello
 				</Typography>
 			</main>
 		</div>
