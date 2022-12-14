451439bef74b08db9fd7bb00660711240fb8264e
risy <therisyy@gmail.com>
2022-12-14 21:44:19 +0300

    -

--- src/views/settings/tabs/password-change.tsx
+++ src/views/settings/tabs/password-change.tsx
@@ -5,27 +5,29 @@ import TextField from "@mui/material/TextField";
 export default function PasswordChangeTab() {
     return (
         <div>
-            <Typography variant="h5" className="text-white">
-                Password and Authentication
-            </Typography>
+            <div className="flex flex-col !gap-y-4">
+                <Typography variant="h5" className="text-white">
+                    Password and Authentication
+                </Typography>
 
-            <TextField
-                placeholder={"Current Password"}
-                type={"password"}
-                className="w-full !mt-4"
-                sx={{ input: { color: 'white' } }}
-            ></TextField>
+                <TextField
+                    placeholder={"Current Password"}
+                    type={"password"}
+                    className="w-full"
+                    sx={{input: {color: 'white'}}}
+                ></TextField>
 
-            <TextField
-                placeholder={"Current Password"}
-                type={"password"}
-                className="w-full !mt-4 !text-white"
-                sx={{ input: { color: 'white' } }}
-            ></TextField>
+                <TextField
+                    placeholder={"Current Password"}
+                    type={"password"}
+                    className="w-full !text-white"
+                    sx={{input: {color: 'white'}}}
+                ></TextField>
 
-            <Button variant="contained" color="primary" className="bg-blue-600 !mt-4">
-                Change Password
-            </Button>
+                <Button variant="contained" color="primary" className="bg-blue-600">
+                    Change Password
+                </Button>
+            </div>
         </div>
     );
 }
